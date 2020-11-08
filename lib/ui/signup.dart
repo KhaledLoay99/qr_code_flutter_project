ea49dd2246aa1c66b4194b8dcc3a1b8a73ddf04a
KhaledLoay99 <45739327+KhaledLoay99@users.noreply.github.com>
2020-11-08 18:12:04 +0200

    update_press

--- lib/ui/signup.dart
+++ lib/ui/signup.dart
@@ -86,12 +86,7 @@ class SignupState extends State<Signup> {
 
                   new Center(
                       child: new RaisedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Login()),
-                          );
-                        },
+                        
                         child: new Text(
                           "Sign Up",
                           style: new TextStyle(
