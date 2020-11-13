fbe0d447657b9261721405028f771451e0aa7b3d
KhaledLoay99 <45739327+KhaledLoay99@users.noreply.github.com>
2020-11-13 23:35:12 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,9 @@
-import 'package:Dcode/ui/privateChat.dart';
 import 'package:flutter/material.dart';
 import 'package:Dcode/ui/signup.dart';
-import './ui/login.dart';
-import './ui/scanQr.dart';
-import './ui/home.dart';
-import './ui/profile.dart';
-import './ui/carProfile.dart';
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-import './ui/privateChat.dart';
-=======
->>>>>>> Stashed changes
-import 'ui/settings.dart';
 
->>>>>>> 370f097b2f0c7505a0ded8185f0ef7a6f7e2f03b
 void main() {
   runApp(new MaterialApp(
     title: "Dcode App",
-    //home: new Login(),
-    home: new Settings(),
-    //home: new home(),
-    //home: new scanQr(),
+    home: new Signup(),
   ));
 }
