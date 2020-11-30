3be58b2d26e818aac347989ae4c9d019bff4d787
Islam Magdy <DashaDeshozz@gmail.com>
2020-11-30 15:53:58 +0200

    cont profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:Dcode/ui/signup.dart';
+import 'package:Dcode/ui/profile.dart';
 
 void main() {
   runApp(new MaterialApp(
     title: "Dcode App",
-    home: new Signup(),
+    home: new Profile(),
   ));
 }
--- lib/ui/profile.dart
+++ lib/ui/profile.dart
@@ -11,6 +11,7 @@ class _ProfileState extends State<Profile> {
   final TextEditingController _controller = new TextEditingController();
   TextFormField test;
   bool _isEnabled;
+  static final validCharacters = RegExp(r"^[a-zA-Z]+$");
   final _formKey = GlobalKey<FormState>();
   TextEditingController _customController;
   createAlertDialog(BuildContext context, String type, String val) {
@@ -22,22 +23,59 @@ class _ProfileState extends State<Profile> {
             title: Text("Your $type"),
             content: Container(
               height: 125,
+              width: 250,
               child: Form(
                 key: _formKey,
                 child: Column(
                   children: [
                     TextFormField(
                       validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Please Enter Your text';
+                        if (type == "FirstName") {
+                          if (value.isEmpty) {
+                            return 'Please Enter First Name';
+                          }
+                          if (value.length < 3) {
+                            return 'First Name is too short';
+                          }
+                          if (value.length > 18) {
+                            return 'First Name is too long';
+                          }
+                          if (!validCharacters.hasMatch(value)) {
+                            return 'First Name should be alphabets only';
+                          }
+                          return null;
                         }
-                        if (value.length > 25) {
-                          return 'too long';
+                        if (type == "LastName") {
+                          if (value.isEmpty) {
+                            return 'Please Enter Last Name';
+                          }
+                          if (value.length < 3) {
+                            return 'Last Name is too short';
+                          }
+
+                          if (value.length > 18) {
+                            return 'Last Name is too long';
+                          }
+                          if (!validCharacters.hasMatch(value)) {
+                            return 'Last Name should be alphabets only';
+                          }
+                          return null;
                         }
-                        if (value.length < 5) {
-                          return 'too short';
+                        if (type == "Email") {
+                          if (value.isEmpty) {
+                            return 'Please Enter Username';
+                          }
+                          if (value.length > 25) {
+                            return 'Username is too long';
+                          }
+                          if (value.length < 11) {
+                            return 'Username is too short';
+                          }
+                          if (!value.contains('@dcode.com')) {
+                            return 'username should end with @decode.com';
+                          }
+                          return null;
                         }
-                        return null;
                       },
                       controller: _customController,
                     ),
@@ -68,18 +106,6 @@ class _ProfileState extends State<Profile> {
         child: Form(
           child: Stack(children: [
             test = TextFormField(
-              /*validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please Enter Your text';
-                }
-                if (value.length > 25) {
-                  return 'too long';
-                }
-                if (value.length < 5) {
-                  return 'too short';
-                }
-                return null;
-              },*/
               enabled: false,
               decoration: InputDecoration(
                   hintText: hintText,
@@ -115,10 +141,6 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
-    /*final _formKey = GlobalKey<FormState>();
-    final _formKey2 = GlobalKey<FormState>();
-    final _formKey3 = GlobalKey<FormState>();
-    final _formKey4 = GlobalKey<FormState>();*/
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: new AppBar(
@@ -127,94 +149,115 @@ class _ProfileState extends State<Profile> {
           icon: Icon(Icons.arrow_back),
           onPressed: () {},
         ),
-//          title: new Text("Login"),
         backgroundColor: const Color.fromRGBO(110, 204, 234, 1.0),
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [],
-          ),
-          CustomPaint(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-            ),
-            painter: HeaderCurvedContainer(),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  "Profile",
-                  style: TextStyle(
-                    fontSize: 35,
-                    letterSpacing: 1.5,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
+      body: ListView(children: [
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Stack(alignment: Alignment.topCenter, children: [
+              CustomPaint(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
                 ),
+                painter: HeaderCurvedContainer(),
               ),
               Container(
-                padding: EdgeInsets.all(10.0),
-                width: MediaQuery.of(context).size.width / 2,
-                height: MediaQuery.of(context).size.width / 2,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white, width: 5),
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    image: DecorationImage(
-                        image: AssetImage('images/profile.jpg'))),
-              )
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 220, left: 184),
-                child: CircleAvatar(
-                  backgroundColor: Colors.black54,
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.edit,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                height: 450,
-                width: double.infinity,
-                margin: EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.topCenter,
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    textfield(
-                        hintText: 'Robert Lewandowski',
-                        qr: false,
-                        type: "Name"),
-                    textfield(
-                        hintText: 'RL9@gmail.com', qr: false, type: "Email"),
-                    textfield(
-                        hintText: 'Warsaw, Poland',
-                        qr: false,
-                        type: "Location"),
-                    textfield(
-                      hintText: 'QR Code',
-                      qr: true,
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        "Profile",
+                        style: TextStyle(
+                          fontSize: 35,
+                          letterSpacing: 1.5,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Stack(
+                      children: [
+                        Container(
+                          //padding: EdgeInsets.all(0.0),
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.width / 2,
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 5),
+                              shape: BoxShape.circle,
+                              color: Colors.red,
+                              image: DecorationImage(
+                                  image: AssetImage('images/profile.jpg'))),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 140, left: 184),
+                          margin: EdgeInsets.only(bottom: 50),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.black54,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
-        ],
-      ),
+              ),
+            ]),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                /*Padding(
+                  padding: EdgeInsets.only(top: 100, left: 184),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black54,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),*/
+                Container(
+                  padding: EdgeInsets.only(top: 150),
+                  height: 550,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      textfield(
+                          hintText: 'Robert', qr: false, type: "FirstName"),
+                      textfield(
+                          hintText: 'Lewandowski', qr: false, type: "LastName"),
+                      textfield(
+                          hintText: 'RL9@dcode.com', qr: false, type: "Email"),
+                      textfield(
+                          hintText: 'Warsaw, Poland',
+                          qr: false,
+                          type: "Location"),
+                      textfield(
+                        hintText: 'QR Code',
+                        qr: true,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }
