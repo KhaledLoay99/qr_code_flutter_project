b31519be69cd21f6ef226fb0398b1bdb5a0254e4
Islam Magdy <DashaDeshozz@gmail.com>
2021-01-13 00:51:26 +0200

    changes to firestore

--- lib/providers/Userprovider.dart
+++ lib/providers/Userprovider.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:Dcode/logic/userProfile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Userprovider with ChangeNotifier {
   Userprovider() {
@@ -29,34 +31,37 @@ class Userprovider with ChangeNotifier {
   }
 
   Future<void> fetchdata() async {
-    const url = "https://dcode-bd3d1-default-rtdb.firebaseio.com/User.json";
+    //const url = "https://dcode-bd3d1-default-rtdb.firebaseio.com/User.json";
+    await Firebase.initializeApp();
+    var snaps = FirebaseFirestore.instance.collection('user');
     try {
-      final http.Response res = await http.get(url);
-      /* http.post(url,
-        body: json.encode({
-          "Email": "RL9@gmail.com",
-          "FirstName": "Robert",
-          "LastName": "Lewandowski",
-          "FirstName": "Robert",
-          "Location": "Wursaw, Poland",
-          "FirstName": "Robert",
-          "profileImage": "images/profile.jpg",
-          "qrImage": "images/car.png",
-        }));*/
-      final extractedData = json.decode(res.body) as Map<String, dynamic>;
-      extractedData.forEach((userid, userData) {
-        user.add(Userprofile(
-          id: userid,
-          email: userData['Email'],
-          firstname: userData['FirstName'],
-          lastname: userData['LastName'],
-          location: userData['Location'],
-          profileImage: userData['profileImage'],
-          qrImage: userData['qrImage'],
-        ));
+      /*snaps.getDocuments().then((QuerySnapshot snapshot) {
+        snapshot.documents.forEach((DocumentSnapshot doc) {
+          print("Hello");
+          print(doc.data);
+        });
+        prog = false;
+        notifyL*/
+      snaps.snapshots().listen((QuerySnapshot querySnapshot) {
+        print('Hello');
+        querySnapshot.documents.forEach((document) {
+          user.add(Userprofile(
+            id: document.documentID,
+            email: document.data()['username'],
+            firstname: document.data()['firstname'],
+            lastname: document.data()['lastname'],
+            location: document.data()['location'],
+            profileImage: document.data()['profileImage'],
+            qrImage: document.data()['qrImage'],
+          ));
+
+          document.data();
+        });
+        snaps.get().then((value) {
+          prog = false;
+          notifyListeners();
+        });
       });
-      prog = false;
-      notifyListeners();
     } catch (error) {
       prog = false;
       err = true;
