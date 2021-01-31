ce6bbcb8fa04d570a6d1641789e453d2883445cc
Abdelazizmuhmd <41162973+Abdelazizmuhmd@users.noreply.github.com>
2021-01-31 12:45:46 +0200

    removed staticobject , added dynamic widget

--- lib/ui/privateChat.dart
+++ lib/ui/privateChat.dart
@@ -43,130 +43,21 @@ class privateChatState extends State<privateChat> {
           padding: const EdgeInsets.only(top: 10.0),
           child: Row(
             children: <Widget>[
-              Icon(Icons.account_circle_rounded),
-              new Text(chatlistLogic.getNames()[0]),
+              Image.network(
+                widget.user['imagepath'],
+                width: 50,
+                height: 50,
+              ),
+              new Text(widget.user['username']),
             ],
           ),
         ),
         backgroundColor: c1,
       ),
       backgroundColor: Colors.white,
-      body: ListView(
-        padding: EdgeInsets.all(30.0),
-        children: [
-          Column(
-            children: <Widget>[
-              new Padding(padding: new EdgeInsets.all(20.0)),
-              new Padding(
-                padding: const EdgeInsets.only(right: 100.0),
-                child: Container(
-                  height: 50,
-                  width: 250,
-                  child: Center(
-                    child: Text(
-                      chatmsgs.getMessages()[0].getText(),
-                      textScaleFactor: 2,
-                      style: TextStyle(color: Colors.white, fontSize: 10),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.blueAccent,
-                  ),
-                ),
-              ),
-              new Text(
-                chatmsgs.getMessages()[0].getTime(),
-                textScaleFactor: 2,
-                style: TextStyle(color: Colors.black26, fontSize: 7),
-              ),
-              new Padding(padding: new EdgeInsets.all(20.0)),
-              new Padding(
-                padding: const EdgeInsets.only(left: 70.0),
-                child: Container(
-                  height: 50,
-                  width: 250,
-                  child: Center(
-                    child: Text(
-                      chatmsgs.getMessages()[1].getText(),
-                      textScaleFactor: 2,
-                      style: TextStyle(color: Colors.white, fontSize: 10),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-              new Text(
-                chatmsgs.getMessages()[1].getTime(),
-                textScaleFactor: 2,
-                style: TextStyle(color: Colors.black26, fontSize: 7),
-              ),
-              new Padding(padding: new EdgeInsets.all(20.0)),
-              new Padding(
-                padding: const EdgeInsets.only(right: 100.0),
-                child: Container(
-                  height: 50,
-                  width: 250,
-                  child: Center(
-                    child: Text(
-                      chatmsgs.getMessages()[2].getText(),
-                      textScaleFactor: 2,
-                      style: TextStyle(color: Colors.white, fontSize: 10),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.blueAccent,
-                  ),
-                ),
-              ),
-              new Text(
-                chatmsgs.getMessages()[2].getTime(),
-                textScaleFactor: 2,
-                style: TextStyle(color: Colors.black26, fontSize: 7),
-              ),
-              new Padding(padding: new EdgeInsets.all(20.0)),
-              new Padding(
-                padding: const EdgeInsets.only(left: 70.0),
-                child: Container(
-                  height: 50,
-                  //width: 250,
-                  child: Center(
-                    child: Text(
-                      chatmsgs.getMessages()[3].getText(),
-                      textScaleFactor: 2,
-                      style: TextStyle(color: Colors.white, fontSize: 10),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-              new Text(
-                chatmsgs.getMessages()[3].getTime(),
-                textScaleFactor: 2,
-                style: TextStyle(color: Colors.black26, fontSize: 7),
-              ),
-              new Padding(padding: new EdgeInsets.all(20.0)),
-              Form(
-                key: _formKey,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.send),
-                    hintText: 'Type . . .',
-                    border: const OutlineInputBorder(),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+      body: chat(),
     );
   }
+
+  Widget chat() {}
 }
