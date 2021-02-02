787d725d15129963532ef0f3fac2f21166ba0067
Ahmed Elbanna <59926177+elbanna962@users.noreply.github.com>
2021-02-03 01:43:53 +0200

    index.js

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const functions = require('firebase-functions');
+const admin= require('firebase-admin');
+
+admin.initializeApp();
+
+exports.myFunction= functions.firestore.document('messages/{m}').onCreate((snapshot,context)=>
+{
+
+	return admin.messaging().sendToTopic('messages',{
+notification :{
+	title: snapshot.data().sentby,
+	body:snapshot.data().text,
+	clickAction:"FLUTTER_NOTIFICATION_CLICK",
+},
+
+
+	});
+});
