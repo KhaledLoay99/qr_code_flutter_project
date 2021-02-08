7095ff3c169b89f758bbc95189628b38925a8466
Ahmed Elbanna <59926177+elbanna962@users.noreply.github.com>
2021-02-08 15:01:50 +0200

    notification done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,56 +1,45 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-
 admin.initializeApp();
-
+const db = admin.firestore();
 const fcm = admin.messaging();
+
+
+
+
 // const db = admin.firestore();
 
 
 export const sendToTopic = functions.firestore
   .document('messages/{messages}')
-
-
   .onCreate(async snapshot => {
+
+
     const dat = snapshot.data();
+
+    const querySnapshot = await db
+      .collection('users')
+      .doc(dat.receivedby)
+      .collection('tokens')
+      .get();
+
+    const tokens = querySnapshot.docs.map(snap => snap.id);
 
     const payload: admin.messaging.MessagingPayload = {
       notification: {
-        title: dat.sentby,
+        title: dat.sendername,
         body: dat.text,
         sound:'defualt',
-        icon:'dcodeLogo.jpg',
-        click_action: 'FLUTTER_NOTIFICATION_CLICK', // required only for onResume or onLaunch callbacks
+        icon:'images/dcodeLogo.jpg',
+        click_action: 'FLUTTER_NOTIFICATION_CLICK',
       },
     };
 
-    return fcm.sendToTopic('messages', payload);
+    return fcm.sendToDevice(tokens, payload);
   });
 
 
 
-// export const sendToDevice = functions.firestore
-//   .document('messages/{messages}')
-//   .onCreate(async snapshot => {
 
 
-//     const order = snapshot.data();
 
-//     const querySnapshot = await db
-//       .collection('users')
-//       .doc(order.seller)
-//       .get();
-
-//     const tokens = querySnapshot.docs.map(snap => snap.id);
-
-//     const payload: admin.messaging.MessagingPayload = {
-//       notification: {
-//         title: 'New Order!',
-//         body: `you sold a ${order.product} for ${order.total}`,
-//         icon: 'your-icon-url',
-//         click_action: 'FLUTTER_NOTIFICATION_CLICK'
-//       }
-//     };
-
-//     return fcm.sendToDevice(tokens, payload);
-//   });
